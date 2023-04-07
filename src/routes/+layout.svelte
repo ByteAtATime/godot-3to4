7e35202353bce5856f9782a62c86f692c7d23d30
ByteAtATime <byteatatime@proton.me>
2023-04-07 04:33:56 +0000

    basic colors & formatting

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,4 +2,8 @@
   import "../app.css";
 </script>
 
-<slot />
+<div class="bg-background-700 text-foreground min-h-screen px-4 py-2">
+  <div class="max-w-screen-lg mx-auto">
+    <slot />
+  </div>
+</div>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{html,js,svelte,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: {
+          700: "#202531",
+        },
+        foreground: "#cee",
+      }
+    },
   },
   plugins: [],
 };
